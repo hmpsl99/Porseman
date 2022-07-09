0d9ef20b54306fc30e981ed2ebb0071c0e931070
Hadi <esmael.c97@gmail.com>
2022-07-09 19:11:16 +0430

    export_add

--- Evaluation/admin.py
+++ Evaluation/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Evaluation
+from .models import Evaluation,Relationship
+from import_export.admin import ExportActionMixin
 # Register your models here.
-admin.site.register(Evaluation)
+class EvaluationAdmin(ExportActionMixin, admin.ModelAdmin):
+    list_display = ('user_logged_in', 'reviewee', 'relationship', 'question','answer','creation_time')
+
+admin.site.register(Evaluation,EvaluationAdmin)
+admin.site.register(Relationship)
