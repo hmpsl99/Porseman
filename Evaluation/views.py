df758d1e136acd658849a1405f6af2d6205fc439
Hadi <esmael.c97@gmail.com>
2022-07-14 18:36:09 +0430

    front_post_api_okay

--- Evaluation/views.py
+++ Evaluation/views.py
@@ -5,6 +5,8 @@ from Employee.models import Employee
 from QA.models import Question,Answer
 from django.http import HttpRequest, HttpResponse, JsonResponse
 import json
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
@@ -17,30 +19,38 @@ def evaluation_page(request):
     return render(request, 'Evaluation/evaluation.html')
 
 def employee_api(request):
-    all_employees = Employee.objects.all().values('first_name')
+    all_employees = Employee.objects.all().values('id','first_name')
     serializer = EmployeeSerializer(all_employees,many = True)
     return JsonResponse(serializer.data,safe=False)
-
+    
+@csrf_protect
 def qa_api(request):
-    query = 'select QA_question.id,QA_question.text ,QA_answer.id as answer_id,QA_answer.text as answer_text from QA_question join QA_answer on QA_answer.question_id =QA_question.id'
-    all_questions_answers = Question.objects.raw(query)
-    length = len(all_questions_answers)
-    final = []
-    for i in range(0,length,2):
-        x = {"q_id":all_questions_answers[i].id,"q_text":all_questions_answers[i].text,"answers":[]}
-        final.append(x)
-    af = []
-    for j in all_questions_answers:
-        keys = ["a_id","a_text"]
-        d = dict.fromkeys(keys)
-        d["a_id"] = j.answer_id
-        d["a_text"] = j.answer_text
-        af.append(d)
-    x = 0 
-    for i in range(len(final)):
-        final[i]["answers"].append(af[x])
-        x+=1
-        final[i]["answers"].append(af[x])
-        x+=1       
-    #serializer = qaSerializer(all_questions_answers, many = True)
-    return JsonResponse(final,safe=False)
+    if request.method == "GET":
+                query = 'select QA_question.id,QA_question.text ,QA_answer.id as answer_id,QA_answer.text as answer_text from QA_question join QA_answer on QA_answer.question_id =QA_question.id'
+                all_questions_answers = Question.objects.raw(query)
+                length = len(all_questions_answers)
+                final = []
+                for i in range(0,length,2):
+                    x = {"q_id":all_questions_answers[i].id,"q_text":all_questions_answers[i].text,"answers":[]}
+                    final.append(x)
+                af = []
+                for j in all_questions_answers:
+                    keys = ["a_id","a_text"]
+                    d = dict.fromkeys(keys)
+                    d["a_id"] = j.answer_id
+                    d["a_text"] = j.answer_text
+                    af.append(d)
+                x = 0 
+                for i in range(len(final)):
+                    final[i]["answers"].append(af[x])
+                    x+=1
+                    final[i]["answers"].append(af[x])
+                    x+=1
+                #persian font problem solved.
+                #a = json.dumps(final, ensure_ascii=False)       
+                #serializer = qaSerializer(all_questions_answers, many = True)
+                return JsonResponse(final,safe=False, json_dumps_params={'ensure_ascii': False})
+    if request.method == "POST":
+                evaluation_serie = str(request.user)
+                return JsonResponse("holly_shit",safe=False)
+    #return HttpResponse(a)
