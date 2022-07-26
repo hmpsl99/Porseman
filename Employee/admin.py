547d7312fa6a2e9b3aa55f48b756bf5c1981908e
Hadi <esmael.c97@gmail.com>
2022-07-26 18:30:36 +0430

    admin_edit_1

--- Employee/admin.py
+++ Employee/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Employee,Department,Team,Position
 # Register your models here.
-my_models = [Employee,Department,Team,Position]
+my_models = [Department,Team,Position]
 admin.site.register(my_models)
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['first_name','last_name','position','team','parent','creation_time','department']
+    sortable_by = ['first_name', 'last_name','position','team','parent','creation_time','department']
+    list_filter = ['first_name', 'last_name','position','team','parent','creation_time','department'] 
+    search_fields = ['first_name','last_name']
+    fields = [('first_name', 'last_name'), 'position','team','parent','creation_time','department']
