3a80bd490afa70c34d9c05fb7214f6cafbe1a8bf
Hadi <esmael.c97@gmail.com>
2022-08-06 11:22:10 +0430

    final_version_insh

--- Evaluation/admin.py
+++ Evaluation/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Evaluation,Relationship
+from .models import Evaluation,Relationship,Results
+from import_export.admin import ExportActionMixin
+
 # Register your models here.
 #class EvaluationAdmin(ExportActionMixin, admin.ModelAdmin):
 #    list_display = ('user_logged_in', 'reviewee', 'relationship', 'question','answer','creation_time')
 
 admin.site.register(Evaluation)
 admin.site.register(Relationship)
+
+@admin.register(Results)
+class ResultsAdmin(ExportActionMixin,admin.ModelAdmin):
+    list_display = ['evaluation_id','reviewee_last_name','question_category','question','reviewee_own_answer','reviewee_own_answer_score','other_avg','difference','department_avg']
+    list_filter = ['reviewee_last_name', 'evaluation_id']
--- Evaluation/models.py
+++ Evaluation/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from QA.models import Question,Answer
 from Employee.models import Employee
 from django.utils import timezone
+from dbview.models import DbView
 # Create your models here.
 class Relationship(models.Model):
     title = models.CharField(max_length=100)
@@ -17,4 +18,19 @@ class Evaluation(models.Model):
     answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
     creation_time = models.DateTimeField(default=timezone.now)
     def __str__(self):
-        return  str(self.question) + ' ' + str(self.answer)
+        return  str(self.question) + ' - ' + str(self.answer)
+
+class Results(models.Model):
+    evaluation_id = models.CharField(max_length=100)
+    reviewee_last_name = models.CharField(max_length=100)
+    question_category = models.CharField(max_length=100)
+    question = models.CharField(max_length=30000)
+    reviewee_own_answer = models.CharField(max_length=30000)
+    reviewee_own_answer_score = models.FloatField()
+    other_avg = models.FloatField()
+    difference = models.FloatField()
+    department_avg =  models.FloatField()
+
+    class Meta:
+        managed = False
+        db_table = "results"
