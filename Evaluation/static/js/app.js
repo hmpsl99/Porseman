e079f9e93379418ba6b82108854c62bcda06451b
Hadi <esmael.c97@gmail.com>
2022-08-18 17:38:05 +0430

    bs-vu-added

--- Evaluation/static/js/app.js
+++ Evaluation/static/js/app.js
@@ -1,27 +1,118 @@
 const app = Vue.createApp({
-    template: `<div v-for="category in datas">
-                    #{{category.category_title}}
-                    <div v-for="question in category.questions">
-                    ---{{question.question_text}}
-                    <div v-for="answer in question.answers">
-                    *******{{answer.answer_text}}
-                    </div>
-                    </div>
-                </div>`,
-    data() {
-        return {
-            datas : []
-        }
-    },
-    methods: {
+  template: `<div class="container-md border border-info mt-3 mb-3 rounded">
+    <form>
+        <div class="container-md text-md-center mt-4 ">
+            *لطفا در فرم زیر ابتدا فرد مورد ارزیابی و سپس نسبت خود با فرد مورد ارزیابی مشخص نمایید*
+        </div>
+        <div class="row m-3">
+            <div class="col">
+                <select class="form-select" aria-label="Default select example">
+                    <option v-for="employee in employees" :value="employee.id">{{employee.first_name}}
+                        {{employee.last_name}}</option>
+                </select>
+            </div>
+            <div class="col">
+                <select class="form-select" aria-label="Default select example">
+                    <option v-for="relationship in relationships" :value="relationship.id">
+                        {{relationship.title}}</option>
+                </select>
+            </div>
+        </div>
+        <div class="container-sm text-md-center mt-4  ">
+            لطفا سؤالات زیر را با دقت پاسخ دهید
+        </div>
+        <div class="container d-block">
+            <div v-for="category in qa">
+                <div class="m-5">
+                    {{category.category_title}}
+                </div>
+                <div v-for="question in category.questions">
+                    {{ question.question_text}}
+                        <div v-for="answer in question.answers" style="display: flex; flex-direction: row; margin-bottom: 10px;" required>
+                            <div st>
+                                <input  :name ="question.question_id" class="form-check-input" type="radio">
+                            </div>
+                            <div class="container">
+                                <label class="form-check-label" >
+                                    {{answer.answer_text}}
+                                  </label>
+                            </div>
+                            
+                          </div>
+                </div>
 
-    },
-    async created() {
-        const response = await fetch("http://127.0.0.1:8000/evaluation/qa_new");
-        const data = await response.json();
-        this.datas = data;
-    }
+            </div>
 
-})
+        </div>
+</div>
 
-app.mount('#app')
+</form>
+</div>`,
+  data() {
+    return {
+      shit: "shit",
+      qa: [
+        {
+          category_title: "shit",
+          questions: [
+            {
+              question_id: 1,
+              question_text: "آیا الفاظش مناسب است؟",
+              answers: [
+                { answer_id: 1, answer_text: "بله" },
+                { answer_id: 2, answer_text: "خیر" },
+              ],
+            },
+            {
+              question_id: 1,
+              question_text: "آیا الفاظش مناسب است؟",
+              answers: [
+                { answer_id: 1, answer_text: "بله" },
+                { answer_id: 2, answer_text: "خیر" },
+              ],
+            },
+          ],
+        },
+        {
+          category_title: "اخلاقی",
+          questions: [
+            {
+              question_id: 1,
+              question_text: "آیا الفاظش مناسب است؟",
+              answers: [
+                { answer_id: 1, answer_text: "بله" },
+                { answer_id: 2, answer_text: "خیر" },
+              ],
+            },
+            {
+              question_id: 1,
+              question_text: "آیا الفاظش مناسب است؟",
+              answers: [
+                { answer_id: 1, answer_text: "بله" },
+                { answer_id: 2, answer_text: "خیر" },
+              ],
+            },
+          ],
+        },
+      ],
+      employees: [
+        { id: 5, first_name: "هومن", last_name: "فهیمی" },
+        { id: 6, first_name: "حسین", last_name: "فلاح" },
+        { id: 7, first_name: "حجت", last_name: "صادقی" },
+      ],
+      relationships: [
+        { id: 1, title: "خودم", score_weight: 0 },
+        { id: 2, title: "همکار مستقیم", score_weight: 2 },
+      ],
+    };
+  },
+  methods: {},
+  // async created() {
+  //     const response_qa = await fetch("http://127.0.0.1:8000/evaluation/qa_new");
+  //     const data = await response_qa.json();
+  //     this.qa = data;
+
+  // }
+});
+
+app.mount("#app");
